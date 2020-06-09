eec6bd043ce569297e9e45c0bb847bb1eff2ac6a
asprince112 <wd3c00@gmail.com>
2020-06-09 18:52:42 +0100

    Q23 - Q25

--- leetcode/21 to 30.py
+++ leetcode/21 to 30.py
@@ -70,3 +70,159 @@ class Solution:
         return -1
 
 ####################################################################################
+'''
+There are n cities numbered from 0 to n-1 and n-1 roads such that there is only one way to travel between two different cities (this network form a tree). Last year, The ministry of transport decided to orient the roads in one direction because they are too narrow.
+
+Roads are represented by connections where connections[i] = [a, b] represents a road from city a to b.
+
+This year, there will be a big event in the capital (city 0), and many people want to travel to this city.
+
+Your task consists of reorienting some roads such that each city can visit the city 0. Return the minimum number of edges changed.
+
+It's guaranteed that each city can reach the city 0 after reorder.
+
+Example 1:
+Input: n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]
+Output: 3
+Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+
+Example 2:
+Input: n = 5, connections = [[1,0],[1,2],[3,2],[3,4]]
+Output: 2
+Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+
+Example 3:
+Input: n = 3, connections = [[1,0],[2,0]]
+Output: 0
+
+Constraints:
+2 <= n <= 5 * 10^4
+connections.length == n-1
+connections[i].length == 2
+0 <= connections[i][0], connections[i][1] <= n-1
+connections[i][0] != connections[i][1]
+'''
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        connections.sort(key=lambda i: (i[0], i[1]))
+        path = set([0])
+        ans = 0
+
+        for start, end in connections:
+            if start in path and end not in path:
+                path.add(end)
+                ans += 1
+            elif end in path and start not in path:
+                path.add(start)
+
+        return ans
+##########################################################################################
+'''
+Given an array of integers arr and an integer k.
+
+A value arr[i] is said to be stronger than a value arr[j] if |arr[i] - m| > |arr[j] - m| where m is the median of the array.
+If |arr[i] - m| == |arr[j] - m|, then arr[i] is said to be stronger than arr[j] if arr[i] > arr[j].
+
+Return a list of the strongest k values in the array. return the answer in any arbitrary order.
+
+Median is the middle value in an ordered integer list. More formally, if the length of the list is n, the median is the element in position ((n - 1) / 2) in the sorted list (0-indexed).
+
+For arr = [6, -3, 7, 2, 11], n = 5 and the median is obtained by sorting the array arr = [-3, 2, 6, 7, 11] and the median is arr[m] where m = ((5 - 1) / 2) = 2. The median is 6.
+For arr = [-7, 22, 17, 3], n = 4 and the median is obtained by sorting the array arr = [-7, 3, 17, 22] and the median is arr[m] where m = ((4 - 1) / 2) = 1. The median is 3.
+
+Example 1:
+Input: arr = [1,2,3,4,5], k = 2
+Output: [5,1]
+Explanation: Median is 3, the elements of the array sorted by the strongest are [5,1,4,2,3]. The strongest 2 elements are [5, 1]. [1, 5] is also accepted answer.
+Please note that although |5 - 3| == |1 - 3| but 5 is stronger than 1 because 5 > 1.
+
+Example 2:
+Input: arr = [1,1,3,5,5], k = 2
+Output: [5,5]
+Explanation: Median is 3, the elements of the array sorted by the strongest are [5,5,1,1,3]. The strongest 2 elements are [5, 5].
+
+Example 3:
+Input: arr = [6,7,11,7,6,8], k = 5
+Output: [11,8,6,6,7]
+Explanation: Median is 7, the elements of the array sorted by the strongest are [11,8,6,6,7,7].
+Any permutation of [11,8,6,6,7] is accepted.
+
+Example 4:
+Input: arr = [6,-3,7,2,11], k = 3
+Output: [-3,11,2]
+
+Example 5:
+Input: arr = [-7,22,17,3], k = 2
+Output: [22,17]
+ 
+
+Constraints:
+1 <= arr.length <= 10^5
+-10^5 <= arr[i] <= 10^5
+1 <= k <= arr.length
+
+'''
+class Solution:
+    def getStrongest(self, arr: List[int], k: int) -> List[int]:
+        if arr == []:
+            return []
+        ans = []
+        l1 = []
+        arr.sort()
+        median = (len(arr) - 1) // 2
+        for i in range(len(arr)):
+            l1.append([abs(arr[i] - arr[median]), arr[i]])
+
+        l1.sort(reverse=True)
+        for j in range(k):
+            ans.append(l1[j][1])
+
+        return ans
+###############################################################################################
+'''
+Given a rectangular cake with height h and width w, and two arrays of integers horizontalCuts and verticalCuts where horizontalCuts[i] is the distance from the top of the rectangular cake to the ith horizontal cut and similarly, verticalCuts[j] is the distance from the left of the rectangular cake to the jth vertical cut.
+
+Return the maximum area of a piece of cake after you cut at each horizontal and vertical position provided in the arrays horizontalCuts and verticalCuts. Since the answer can be a huge number, return this modulo 10^9 + 7.
+
+Example 1:
+Input: h = 5, w = 4, horizontalCuts = [1,2,4], verticalCuts = [1,3]
+Output: 4 
+Explanation: The figure above represents the given rectangular cake. Red lines are the horizontal and vertical cuts. After you cut the cake, the green piece of cake has the maximum area.
+
+Example 2:
+Input: h = 5, w = 4, horizontalCuts = [3,1], verticalCuts = [1]
+Output: 6
+Explanation: The figure above represents the given rectangular cake. Red lines are the horizontal and vertical cuts. After you cut the cake, the green and yellow pieces of cake have the maximum area.
+
+Example 3:
+Input: h = 5, w = 4, horizontalCuts = [3], verticalCuts = [3]
+Output: 9
+ 
+
+Constraints:
+2 <= h, w <= 10^9
+1 <= horizontalCuts.length < min(h, 10^5)
+1 <= verticalCuts.length < min(w, 10^5)
+1 <= horizontalCuts[i] < h
+1 <= verticalCuts[i] < w
+It is guaranteed that all elements in horizontalCuts are distinct.
+It is guaranteed that all elements in verticalCuts are distinct.
+'''
+class Solution:
+    def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+        horizontalCuts += [0, h]
+        verticalCuts += [0, w]
+        horizontalCuts.sort()
+        verticalCuts.sort()
+        h_list, w_list  = [], []
+        ans = 0
+        
+        for i in range(1, len(horizontalCuts)):
+            h_list.append(horizontalCuts[i] - horizontalCuts[i-1])
+        
+        for j in range(1, len(verticalCuts)):
+            w_list.append(verticalCuts[j] - verticalCuts[j-1])
+        
+        ans = max(h_list) * max(w_list)
+        return ans % (pow(10,9) + 7)
+###############################################################################################
