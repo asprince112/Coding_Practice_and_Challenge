ff6b23004770ca1b3f1650c1aecbe924925b0d7b
asprince112 <wd3c00@gmail.com>
2020-07-14 11:43:52 +0100

    35

--- leetcode/31 to 40.py
+++ leetcode/31 to 40.py
@@ -116,3 +116,30 @@ class Solution:
 
         return get(n, k, l1)
 ######################################################################################
+'''
+Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+Example:
+
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+
+return 13.
+Note:
+You may assume k is always valid, 1 ≤ k ≤ n2.
+'''
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        l1 = []
+        for i in matrix:
+            l1.extend(i)
+
+        return sorted(l1)[k-1]
+################################################################################
+    
